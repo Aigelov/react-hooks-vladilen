a4227639a31e2a9c8c094dcc912ed8c795af7a0c
Galymzhan Aigelov <galymzhan@otgroup.kz>
2020-06-24 10:40:42 +0600

    Alert hook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React from 'react'
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
+import { Navbar } from './components/Navbar'
+import { Home } from './pages/Home'
+import { About } from './pages/About'
+import { Profile } from './pages/Profile'
+import { Alert } from './components/Alert'
+import { AlertState } from './context/alert/AlertState'
 
 function App() {
   return (
-    <div className='container pt-4'>
-      <h2>Hello world</h2>
-    </div>
+    <AlertState>
+      <BrowserRouter>
+        <Navbar />
+        <div className='container pt-4'>
+          <Alert />
+          <Switch>
+            <Route path={'/'} exact component={Home} />
+            <Route path={'/about'} component={About} />
+            <Route path={'/profile/:name'} component={Profile} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    </AlertState>
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Search } from '../components/Search'
+import { Card } from '../components/Card'
+
+export const Home = () => {
+  const cards = new Array(9)
+    .fill('')
+    .map((_, index) => index)
+
+  return (
+    <React.Fragment>
+      <Search />
+
+      <div className='row'>
+        { cards.map(card => {
+          return (
+            <div className='col-sm-4 mb-4' key={card}>
+              <Card />
+            </div>
+          )
+        }) }
+      </div>
+    </React.Fragment>
+  )
+}
